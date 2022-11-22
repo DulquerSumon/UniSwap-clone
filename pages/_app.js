86a3d699da1562a13855838cccd5bfb5f052a803
DulquerSumon <m.dulquer.sumon@gmail.com>
2022-11-23 01:32:15 +0600

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+// import { MoralisProvider } from "react-moralis";
+import { TransactionProvider } from "../context/TransactionContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <TransactionProvider>
+      {/* <MoralisProvider initializeOnMount={false}> */}
+      <Component {...pageProps} />
+      {/* </MoralisProvider> */}
+    </TransactionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
